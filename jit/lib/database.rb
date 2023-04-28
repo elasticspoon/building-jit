05d375dbe4437416b770ade2d191673aa69592c7
Yuri Bocharov <quesadillaman@gmail.com>
2023-04-28 00:01:58 -0400

    small update

--- jit/lib/database.rb
+++ jit/lib/database.rb
@@ -1,21 +1,21 @@
-require "digest/sha1"
-require "fileutils"
-require "zlib"
-require "strscan"
+require 'digest/sha1'
+require 'fileutils'
+require 'zlib'
+require 'strscan'
 
-require_relative "./database/blob"
-require_relative "./database/commit"
-require_relative "./database/tree"
-require_relative "./database/author"
-require_relative "./database/entry"
-require_relative "./database/tree_diff"
+require_relative './database/blob'
+require_relative './database/commit'
+require_relative './database/tree'
+require_relative './database/author'
+require_relative './database/entry'
+require_relative './database/tree_diff'
 
 class Database
-  TEMP_CHARS = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
+  TEMP_CHARS = ('a'..'z').to_a + ('A'..'Z').to_a + ('0'..'9').to_a
   OBJECT_TYPES = {
-    "commit" => Commit,
-    "tree" => Tree,
-    "blob" => Blob
+    'commit' => Commit,
+    'tree' => Tree,
+    'blob' => Blob
   }.freeze
 
   def initialize(pathname)
